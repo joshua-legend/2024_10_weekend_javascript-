d6f896f0fa57c013f05735bfb6246bd7b369485a
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-11-16 13:50:37 +0900

    Add files via upload

---
+++ Day08/review/index.js
@@ -0,0 +1,7 @@
+const layout = document.querySelector(".layout");
+const makeButton = (className) => `<button class="button ${className}">Button</button>`;
+
+layout.insertAdjacentHTML("beforeend", makeButton("seaBlue"));
+layout.insertAdjacentHTML("beforeend", makeButton("deepBlue"));
+layout.insertAdjacentHTML("beforeend", makeButton("white"));
+layout.insertAdjacentHTML("beforeend", makeButton("ligthBlue"));
